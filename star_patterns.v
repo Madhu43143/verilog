bee62a7ac8deace59f1e2f3118c0a5e324a71717
Madhu <126952592+Madhu43143@users.noreply.github.com>
2026-04-05 14:57:49 +0530

    Update star_patterns.v

--- star_patterns.v
+++ star_patterns.v
@@ -1,13 +1,17 @@
 
 //////////////////////////////////  star patterns in simulation time///////////////////////////////////////////////////////
+`timescale 1 ns / 1 ps
+
 module tb();
 integer i,j;
 
 initial begin
-  for(i=0;i<=5;i=i+1)begin
-    for(j=0;j<i;j=j+1)
+  for(i=0;i<=5;i=i+1)begin // rows
+    for(j=0;j<i;j=j+1) // column
       $write("*");
+		$display();
   end
-  $display();
+  
 end
 endmodule
+
