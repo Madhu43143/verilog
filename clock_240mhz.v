43e73352cc84f2fa54b27cd517fd92a27e1f5068
Madhu <126952592+Madhu43143@users.noreply.github.com>
2026-04-07 23:06:34 +0530

    Update clock_240mhz.v

--- clock_240mhz.v
+++ clock_240mhz.v
@@ -16,11 +16,14 @@ module tb();
   clk=0;
   out=0;
     forever begin
-    #1  out = 1;
-    #0.001 out = 0;
+    #1  out = 1; // 1us out=1;
+    #0.001 out = 0;// output high only at 0.001 us
     end
   end
-  always #0.002 clk = ~clk;  // 0.002
+  always #0.002 clk = ~clk;  // 0.002  one complete clock cycle = 0.004 => one complete cycle = 0.004us * 10-6 --> 4ns *10^-9 ==> one clock cycle = 4ns 
+                             // 5*4 = 20ns => 5clock cycles complete  = 20ns 
+                             //1us = 1000ns,so we see upto 1000ns ==> 250*4 = 1000ns
+                            // output will be detected at 250clock cycles
 endmodule
 
 
